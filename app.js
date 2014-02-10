012fa2b430374c770fb1876d8d1c40bd816c66ac
Zubair Lawrence <zubairla@lizardpro.com>
2014-02-09 23:07:48 -0800

    run command

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+var sys = require('sys');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+function start() {
+    exec("/usr/local/bin/gpio -g pwm 18 10", puts);
+}
+function stop() {
+    exec("/usr/local/bin/gpio -g pwm 18 0", puts);
+}
+
 
 app.get('/', function (req, res) {
-    res.send('command: ' + req.query.id);
-});
+    res.send('command: ' + req.query.command);
+    switch (req.query.command) {
+        case "start":
+            start();
+            break;
+        case "stop":
+            stop();
+            break;
+        default:
+            break;
 
+    }
+});
+console.log("hello world");
 app.listen(3000);
